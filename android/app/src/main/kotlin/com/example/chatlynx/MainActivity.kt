424b35cb0ae2fc4de46f1781f8e86b1d744d984c
Juan Francisco Téllez Rico <Jtellez@etbsa.local>
2024-06-06 13:47:19 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/chatlynx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatlynx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/auth/login_or_register.dart
@@ -0,0 +1,27 @@
+import 'package:chatlynx/pages/Loginpage.dart';
+import 'package:chatlynx/pages/Registerpage.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegister();
+}
+
+class _LoginOrRegister extends State<LoginOrRegister> {
+  bool showLoginPage =true;
+  void togglePages(){
+    setState(() {
+      showLoginPage=!showLoginPage;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    if(showLoginPage){
+      return LoginPage(onTap: togglePages);
+    }else{
+      return RegisterPage(onTap: togglePages);
+    }
+  }
+}
